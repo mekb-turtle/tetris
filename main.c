23d5b72acc586692b7bdbef85f2b4f6d1f092d3b
mekb the turtle <mekbgithub@icloud.com>
2022-06-07 17:49:53 +1000

    update

--- main.c
+++ main.c
@@ -7,12 +7,28 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#define GAME_W 10
-#define GAME_H 30
-uint8_t board[GAME_H][GAME_W];
-uint8_t place[GAME_H][GAME_W];
+#include <signal.h>
+#include <termios.h>
+#define RANDOM_FILE "/dev/urandom"
+#define DELAY 10
+#define MOVE_EVERY 30
+#define GAME_I 20
+#define GAME_J 10
+#define BELL // comment out if you don't want a bell
+uint8_t game_over = 0;
+FILE* rng;
+uint8_t place_indicator[GAME_I][GAME_J];
+uint8_t board[GAME_I][GAME_J];
+uint8_t place[GAME_I][GAME_J];
 #include "./tetrominos.h"
+#define COLOR_BORDER "\x1b[38;5;8m"
 #define RESET "\x1b[0m"
+#define BOARD_CHAR "██"
+#define PLACE_CHAR "██"
+#define INDICATOR_CHAR "▒▒"
+#define BORDER_CHAR "▒▒"
+#define EMPTY_CHAR "  "
+#define BORDER RESET COLOR_BORDER BORDER_CHAR
 uint8_t print_color(uint8_t col) {
 	if      (col == ID_I) printf(COLOR_I);
 	else if (col == ID_J) printf(COLOR_J);
@@ -26,50 +42,58 @@ uint8_t print_color(uint8_t col) {
 }
 void render() {
 	printf("\x1b[H");
-	for (size_t i = 0; i < GAME_H; ++i) {
-		for (size_t j = 0; j < GAME_W; ++j) {
-			printf(RESET);
-			if (print_color(board[i][j]))
-				print_color(place[i][j]);
-			printf(" ");
+	for     (uint8_t j = 0; j < GAME_J+2; ++j) printf(BORDER);
+	printf("\r\n");
+	for     (uint8_t i = 0; i < GAME_I; ++i) {
+		printf(BORDER RESET);
+		for (uint8_t j = 0; j < GAME_J; ++j) {
+			if (print_color(board[i][j])) {
+				printf(BOARD_CHAR);
+			} else if (print_color(place[i][j])) {
+				printf(PLACE_CHAR);
+			} else if (print_color(place_indicator[i][j])) {
+				printf(INDICATOR_CHAR);
+			} else {
+				printf(EMPTY_CHAR);
+			}
 		}
-		printf("\n");
+		printf(BORDER "\r\n");
 	}
+	for     (uint8_t j = 0; j < GAME_J+2; ++j) printf(BORDER);
+	printf("\r\n\r\n" RESET "\
+\x1b[7mA\x1b[27m to move left\r\n\
+\x1b[7mD\x1b[27m to move right\r\n\
+\x1b[7mS\x1b[27m to move down quicker\r\n\
+\x1b[7mQ\x1b[27m to rotate anti-clockwise\r\n\
+\x1b[7mE\x1b[27m to rotate clockwise\r\n\
+\x1b[7mSpace\x1b[27m to drop instantly\r\n\
+");
 }
 uint8_t is_placing() {
-	for (size_t i = 0; i < GAME_H; ++i) {
-		for (size_t j = 0; j < GAME_W; ++j) {
+	for     (uint8_t i = 0; i < GAME_I; ++i)
+		for (uint8_t j = 0; j < GAME_J; ++j)
 			if (place[i][j] != 0) return 1;
-		}
-	}
 	return 0;
 }
-uint8_t place_block(uint8_t id, uint8_t** piece, uint8_t I, uint8_t J) {
-	if (is_placing()) return 1;
-	size_t i_ = 0;
-	size_t j_ = (GAME_W-J)/2;
-	for (size_t i = 0; i < I; ++i) {
-		for (size_t j = 0; j < J; ++j) {
-			if (piece[i][j])
+void add_block(uint8_t id, uint8_t** piece, uint8_t I, uint8_t J) {
+	uint8_t i_ = 0;
+	uint8_t j_ = (GAME_J-J)/2;
+	for     (uint8_t i = 0; i < I; ++i)
+		for (uint8_t j = 0; j < J; ++j)
+			if (piece[i][j]) {
+				if (board[i_+i][j_+j])
+					game_over = 1;
 				place[i_+i][j_+j] = id;
-		}
-	}
-	return 0;
+			}
 }
-uint8_t place_random_block() {
-	uint8_t t = rand() % 7;
-	place_block(
+void add_random_block() {
+	uint8_t t = getc(rng) % 7;
+	add_block(
 		t==0?ID_I:t==1?ID_J:t==2?ID_L:t==3?ID_O:t==4?ID_S:t==5?ID_T:ID_Z,
 		t==0?piece_i:t==1?piece_j:t==2?piece_l:t==3?piece_o:t==4?piece_s:t==5?piece_t:piece_z,
 		t==0?I_I:t==1?I_J:t==2?I_L:t==3?I_O:t==4?I_S:t==5?I_T:I_Z,
 		t==0?J_I:t==1?J_J:t==2?J_L:t==3?J_O:t==4?J_S:t==5?J_T:J_Z
 	);
-}
-void clear() {
-	printf("\x1b[H\x1b[J\x1b[2J");
-}
-void logic() {
-	
 }
 void sleep_(unsigned long ms) {
 	struct timespec r = { ms/1e3, (ms%1000)*1e6 };
@@ -78,20 +102,149 @@ void sleep_(unsigned long ms) {
 unsigned char poll_() {
 	struct pollfd fds;
 	unsigned char ret;
-	fds.fd = STDIN_FILENO;
+	fds.fd = fileno(stdin);
 	fds.events = POLLIN;
 	ret = poll(&fds, 1, 0);
 	return ret;
 }
-void step() {
-	while (poll_()) {
-		unsigned char c = getc(stdin);
+uint8_t move(int8_t move_i, int8_t move_j) {
+	if (move_i == 0 && move_j == 0) return 1;
+	if (move_i < -1 || move_i > 1 || move_j < -1 || move_j > 1) return 0;
+	for     (uint8_t i = 0; i < GAME_I; ++i)
+		for (uint8_t j = 0; j < GAME_J; ++j)
+			if (place[i][j]) {
+				if (move_i < 0 && i == 0) return 0;
+				if (move_i > 0 && i >= GAME_I-1) return 0;
+				if (move_j < 0 && j == 0) return 0;
+				if (move_j > 0 && j >= GAME_J-1) return 0;
+				if (board[i+move_i][j+move_j]) return 0;
+			}
+	for     (uint8_t i = move_i >= 0 ? GAME_I - 1 : 0; move_i >= 0 ? (i >= 0 && i != UINT8_MAX) : i < GAME_I; i += move_i >= 0 ? -1 : 1)
+		for (uint8_t j = move_j >= 0 ? GAME_J - 1 : 0; move_j >= 0 ? (j >= 0 && j != UINT8_MAX) : j < GAME_J; j += move_j >= 0 ? -1 : 1)
+			if (place[i][j]) {
+				place[i+move_i][j+move_j] = place[i][j];
+				place[i][j] = 0;
+			}
+	return 1;
+}
+uint8_t move_indicator() {
+	for     (uint8_t i = 0; i < GAME_I; ++i)
+		for (uint8_t j = 0; j < GAME_J; ++j)
+			place_indicator[i][j] = place[i][j];
+	uint8_t break_ = 0;
+	while (!break_) {
+		for     (uint8_t i = 0; !break_ && i < GAME_I; ++i)
+			for (uint8_t j = 0; !break_ && j < GAME_J; ++j)
+				if (place_indicator[i][j]) {
+					if (i >= GAME_I-1) { break_ = 1; break; }
+					if (board[i+1][j]) { break_ = 1; break; }
+				}
+		if (break_) break;
+		for     (uint8_t i = GAME_I - 1; i >= 0 && i != UINT8_MAX; --i)
+			for (uint8_t j = 0; j < GAME_J; ++j)
+				if (place_indicator[i][j]) {
+					place_indicator[i+1][j] = place_indicator[i][j];
+					place_indicator[i][j] = 0;
+				}
 	}
+	return 1;
+}
+uint8_t place_to_board() {
+	uint8_t g = 0;
+	for     (uint8_t i = 0; i < GAME_I; ++i)
+		for (uint8_t j = 0; j < GAME_J; ++j)
+			if (place[i][j]) {
+				board[i][j] = place[i][j];
+				place[i][j] = 0;
+				g = 1;
+			}
+	return g;
+}
+uint8_t move_timer = 0;
+void clear() {
+	printf("\x1b[H\x1b[J\x1b[2J\x1b[0m");
+}
+void begin() {
+	system("stty raw");
+	printf("\x1b[s\x1b[?47h\x1b[?25l");
+	clear();
+    struct termios term;
+    tcgetattr(fileno(stdin), &term);
+    term.c_lflag &= ~(ECHO|ISIG);
+    tcsetattr(fileno(stdin), 0, &term);
+}
+void end() {
+    struct termios term;
+    tcgetattr(fileno(stdin), &term);
+    term.c_lflag |= ECHO|ISIG;
+    tcsetattr(fileno(stdin), 0, &term);
+	clear();
+	printf("\x1b[?47l\x1b[?25h\x1b[u");
+	system("stty sane");
+}
+void end_and_exit() {
+	end();
+	exit(0);
+}
+void bell() {
+#ifdef BELL
+	printf("\a");
+#endif
 }
 int main() {
-	place_random_block();
-	clear();
-	logic();
-	render();
+	rng = fopen(RANDOM_FILE, "r");
+	if (!rng) {
+		fprintf(stderr, "%s: %s", RANDOM_FILE, strerror(errno));
+		return 1;
+	}
+	create_pieces();
+	begin();
+	signal(SIGINT,  end_and_exit);
+	signal(SIGHUP,  end_and_exit);
+	signal(SIGQUIT, end_and_exit);
+	signal(SIGKILL, end_and_exit);
+	signal(SIGTERM, end_and_exit);
+	signal(SIGSTOP, end_and_exit);
+	signal(SIGSEGV, end_and_exit);
+	signal(SIGILL,  end_and_exit);
+	add_random_block();
+	move_indicator();
+	bell();
+	while (1) {
+		if (++move_timer >= MOVE_EVERY) {
+			move_timer = 0;
+			if (!move(1, 0)) {
+				bell();
+				place_to_board();
+				//add_random_block();
+				move_indicator();
+			}
+		}
+		while (poll_()) {
+			int c_ = fgetc(stdin);
+			if (c_ == EOF) {
+				end();
+				clear();
+				fprintf(stderr, "EOF\n");
+				return 2;
+			}
+			char c = c_;
+			if (c == '\x1a' || c == '\x03' || c == '\x04') { end(); return 0; }
+			else if (c == '\x1b') break;
+			else if (c == 'a' || c == 'A') { move(0, -1); move_indicator(); }
+			else if (c == 'd' || c == 'D') { move(0,  1); move_indicator(); }
+			else if (c == 's' || c == 'S') { move(1,  0); }
+		}
+		render();
+		if (game_over) {
+			bell();
+			end();
+			clear();
+			printf("Game Over!\n");
+			return 0;
+		}
+		sleep_(DELAY);
+	}
+	end();
 	return 0;
 }
--- tetrominos.h
+++ tetrominos.h
@@ -12,20 +12,60 @@
 #define J_T 2
 #define I_Z 3
 #define J_Z 2
-uint8_t piece_i[I_I][J_I] = {{1},{1},{1},{1}};
-uint8_t piece_j[I_J][J_J] = {{0,1},{0,1},{1,1}};
-uint8_t piece_l[I_L][J_L] = {{1,0},{1,0},{1,1}};
-uint8_t piece_o[I_O][J_O] = {{1,1},{1,1}};
-uint8_t piece_s[I_S][J_S] = {{1,0},{1,1},{0,1}};
-uint8_t piece_t[I_T][J_T] = {{1,0},{1,1},{1,0}};
-uint8_t piece_z[I_Z][J_Z] = {{0,1},{1,1},{1,0}};
-#define COLOR_I "\x1b[48;5;14m"
-#define COLOR_J "\x1b[48;5;12m"
-#define COLOR_L "\x1b[48;5;3m"
-#define COLOR_O "\x1b[48;5;11m"
-#define COLOR_S "\x1b[48;5;10m"
-#define COLOR_T "\x1b[48;5;13m"
-#define COLOR_Z "\x1b[48;5;9m"
+uint8_t** piece_i;
+uint8_t** piece_j;
+uint8_t** piece_l;
+uint8_t** piece_o;
+uint8_t** piece_s;
+uint8_t** piece_t;
+uint8_t** piece_z;
+
+void create_pieces() {
+	piece_i = malloc(sizeof(uint8_t*)*I_I);
+	piece_j = malloc(sizeof(uint8_t*)*I_J);
+	piece_l = malloc(sizeof(uint8_t*)*I_L);
+	piece_o = malloc(sizeof(uint8_t*)*I_O);
+	piece_s = malloc(sizeof(uint8_t*)*I_S);
+	piece_t = malloc(sizeof(uint8_t*)*I_T);
+	piece_z = malloc(sizeof(uint8_t*)*I_Z);
+
+	for (size_t i = 0; i < I_I; ++i) { piece_i[i] = malloc(sizeof(uint8_t)*J_I); }
+	for (size_t i = 0; i < I_J; ++i) { piece_j[i] = malloc(sizeof(uint8_t)*J_J); }
+	for (size_t i = 0; i < I_L; ++i) { piece_l[i] = malloc(sizeof(uint8_t)*J_L); }
+	for (size_t i = 0; i < I_O; ++i) { piece_o[i] = malloc(sizeof(uint8_t)*J_O); }
+	for (size_t i = 0; i < I_S; ++i) { piece_s[i] = malloc(sizeof(uint8_t)*J_S); }
+	for (size_t i = 0; i < I_T; ++i) { piece_t[i] = malloc(sizeof(uint8_t)*J_T); }
+	for (size_t i = 0; i < I_Z; ++i) { piece_z[i] = malloc(sizeof(uint8_t)*J_Z); }
+
+	piece_i[0][0] = piece_i[1][0] = piece_i[2][0] = piece_i[3][0] = 1;
+
+	piece_j[0][0] = piece_j[1][0] = 0;
+	piece_j[0][1] = piece_j[1][1] = piece_j[2][1] = piece_j[2][0] = 1;
+
+	piece_l[0][1] = piece_l[1][1] = 0;
+	piece_l[0][0] = piece_l[1][0] = piece_l[2][0] = piece_l[2][1] = 1;
+
+	piece_o[0][0] = piece_o[0][1] = piece_o[1][0] = piece_o[1][1] = 1;
+
+	piece_s[0][1] = piece_s[2][0] = 0;
+	piece_s[0][0] = piece_s[1][0] = piece_s[1][1] = piece_s[2][1] = 1;
+
+	piece_t[0][1] = piece_t[2][1] = 0;
+	piece_t[0][0] = piece_t[1][0] = piece_t[1][1] = piece_t[2][0] = 1;
+
+	piece_z[0][0] = piece_z[2][1] = 0;
+	piece_z[0][1] = piece_z[1][0] = piece_z[1][1] = piece_z[2][0] = 1;
+}
+
+#define COLOR_I "\x1b[38;5;14m"
+#define COLOR_J "\x1b[38;5;12m"
+#define COLOR_L "\x1b[38;5;3m"
+#define COLOR_O "\x1b[38;5;11m"
+#define COLOR_S "\x1b[38;5;10m"
+#define COLOR_T "\x1b[38;5;13m"
+#define COLOR_Z "\x1b[38;5;9m"
+
+// do not put 0, it is treated as no block
 #define ID_I 1
 #define ID_J 2
 #define ID_L 3
